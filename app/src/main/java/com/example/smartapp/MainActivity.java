df896aaea114be28581ab311c4886ef2c75f2ed3
syeda insharah <syedainsharahnaveed@gmail.com>
2020-04-03 20:15:40 +0500

    upload

---
+++ app/src/main/java/com/example/smartapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.smartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent = new Intent(MainActivity.this,loginactivity.class);
+                startActivity(homeintent);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
